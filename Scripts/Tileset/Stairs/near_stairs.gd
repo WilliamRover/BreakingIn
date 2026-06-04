8bcdce2bc058b5341ddb66d14f59056e05a07707
William Rover <roverwilliam4567@gmail.com>
2026-06-04 14:28:40 +0300

    STAIRS WORKING OMFG

--- Scripts/Tileset/Stairs/near_stairs.gd
+++ Scripts/Tileset/Stairs/near_stairs.gd
@@ -3,6 +3,7 @@ class_name NearStairs extends Node2D
 
 @onready var bottom: Area2D = $Bot
 @onready var top: Area2D = $Top
+@onready var mid: Area2D = $StairZone
 
 @export var floor0Layer := 1
 @export var floor1Layer := 2
@@ -10,35 +11,42 @@ class_name NearStairs extends Node2D
 
 func _on_top_body_entered(body: Node2D) -> void:
 	if body is Player:
-		body.z_index = 1
-		body.set_collision_mask_value(floor1Layer, true)
-		body.set_collision_mask_value(stairsLayer, true)
+		if body.get_collision_mask_value(floor1Layer) or body.get_collision_mask_value(stairsLayer):
+			body.z_index = 1
+			body.set_collision_mask_value(floor1Layer, true)
+			body.set_collision_mask_value(stairsLayer, true)
 
 
 func _on_top_body_exited(body: Node2D) -> void:
 	if body is Player:
-		if body.global_position.y < top.global_position.y:
-			body.set_collision_mask_value(stairsLayer, false)
-		else:
-			#body.z_index = 0
-			body.set_collision_mask_value(floor1Layer, false)
+		if body.get_collision_mask_value(floor1Layer) or body.get_collision_mask_value(stairsLayer):
+			if mid.overlaps_body(body):
+				print("top tru ex")
+				body.set_collision_mask_value(floor1Layer, false)
+			else:
+				print("top f ex")
+				#body.z_index = 0
+				body.set_collision_mask_value(stairsLayer, false)
+			
 
 
 func _on_bot_body_entered(body: Node2D) -> void:
 	if body is Player:
-		print("bot enter")
-		body.z_index = 1
-		body.set_collision_mask_value(floor0Layer, true)
-		body.set_collision_mask_value(stairsLayer, true)
+		if body.get_collision_mask_value(floor0Layer) or body.get_collision_mask_value(stairsLayer):
+		#print("bot enter")
+			body.z_index = 1
+			body.set_collision_mask_value(floor0Layer, true)
+			body.set_collision_mask_value(stairsLayer, true)
 
 
 func _on_bot_body_exited(body: Node2D) -> void:
 	if body is Player:
-		if body.global_position.y < bottom.global_position.y:
-			print("bot tru ex")
-			body.set_collision_mask_value(floor0Layer, false)
-		else:
-			print("bot f ex")
-			body.z_index = 0
-			#body.set_collision_mask_value(floor0Layer, false)
-			body.set_collision_mask_value(stairsLayer, false)
+		if body.get_collision_mask_value(floor0Layer) or body.get_collision_mask_value(stairsLayer):
+			if mid.overlaps_body(body):
+				print("bot tru ex")
+				body.set_collision_mask_value(floor0Layer, false)
+			else:
+				print("bot f ex")
+				body.z_index = 0
+				#body.set_collision_mask_value(floor0Layer, false)
+				body.set_collision_mask_value(stairsLayer, false)
